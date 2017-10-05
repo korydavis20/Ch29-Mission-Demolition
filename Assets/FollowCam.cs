8519d91805d265e8cdb81928453e1fe15f1cbc34
Kory Davis <ked130@scarletmail.rutgers.edu>
2017-10-05 12:26:24 -0400

    Step 5 - Camera Updates
    Updated the FollowCam script to minimize projectile jitters and screen
    panning.

--- Assets/FollowCam.cs
+++ Assets/FollowCam.cs
@@ -6,6 +6,8 @@ public class FollowCam : MonoBehaviour {
 	static public FollowCam S; //a followCam Singleton
 
 	//fields set in the unity inspector pane
+	public float easing = 0.05f;
+	public Vector2 minXY;
 	public bool ___________________________________________________________________;
 	//fields set dynamically
 	public GameObject poi; // point of interest
@@ -22,7 +24,7 @@ public class FollowCam : MonoBehaviour {
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	void FixedUpdate () {
 		//there's only one line following an if, if doesn't need braces
 		if (poi == null) { //if there's no poi
 			return;
@@ -30,9 +32,16 @@ public class FollowCam : MonoBehaviour {
 
 		//get the position of poi
 		Vector3 destination = poi.transform.position;
+		//limit the x and y to minimum values
+		destination.x = Mathf.Max(minXY.x, destination.x);
+		destination.y = Mathf.Max(minXY.y, destination.y);
+		//interpolate from the current camera position toward destination
+		destination = Vector3.Lerp(transform.position, destination, easing);
 		//retain a destination.z of camZ
 		destination.z = camZ;
 		//set the camera to the destination
 		transform.position = destination;
+		//set the orthrographic size of the camera to keep ground in view
+		this.GetComponent<Camera>().orthographicSize = destination.y + 10;
 	}
 }
