05b97dbdd6ef900346727bb437d60f69ef1de396
Kory Davis <ked130@scarletmail.rutgers.edu>
2017-10-04 21:55:42 -0400

    Step 4 - FollowCam
    Added the script FollowCam to make the gamera follow projectile movement
    by getting the transform of poi and setting the camera to that position
    on each update.

---
+++ Assets/FollowCam.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowCam : MonoBehaviour {
+	static public FollowCam S; //a followCam Singleton
+
+	//fields set in the unity inspector pane
+	public bool ___________________________________________________________________;
+	//fields set dynamically
+	public GameObject poi; // point of interest
+	public float camZ; //desired Z position of the camera
+
+	void Awake(){
+		S = this;
+		camZ = this.transform.position.z;
+	}
+		
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//there's only one line following an if, if doesn't need braces
+		if (poi == null) { //if there's no poi
+			return;
+		}
+
+		//get the position of poi
+		Vector3 destination = poi.transform.position;
+		//retain a destination.z of camZ
+		destination.z = camZ;
+		//set the camera to the destination
+		transform.position = destination;
+	}
+}
